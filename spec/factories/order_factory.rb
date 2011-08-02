8a9afba23dbd3a78a9e41696cbb43d2374f59f19
Joshua Jacobs <josh@mandala-designs.com>
2011-08-02 00:27:33 -0400

    refactored for clarity

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -4,9 +4,15 @@ FactoryGirl.define do
     on_hand 1000
   end
 
+  factory :order_address, :aliases => [:bill_address, :ship_address], :parent => :address
+
   factory :completed_order, :parent => :order do
-    association :bill_address, :factory => :address
-    association :ship_address, :factory => :address
+    #noinspection RubyResolve
+    bill_address
+
+    #noinspection RubyResolve
+    ship_address
+
     shipping_method
 
     # add line items
@@ -14,29 +20,23 @@ FactoryGirl.define do
 
     after_create do |order|
 
-      # cart -> address
-      order.next
+      order.next # cart -> address
+      order.next # address -> delivery
+      order.next # delivery -> payment
 
-      # address -> delivery
-      order.next
-
-      # delivery -> payment
       payment = order.payments.create(
           :amount => order.total,
           :source => Factory(:creditcard),
           :payment_method => Factory(:bogus_payment_method)
       )
 
-      order.next
+      order.next # payment -> confirm
+      order.next # confirm -> complete
 
-      # confirm -> complete
-      order.next
-
-      # complete
-      order.next
-
+      # simulate CC capture
       payment.complete
 
+      # update totals
       order.update!
     end
 
