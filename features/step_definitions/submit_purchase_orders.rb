42cf4a297754ce8f79d9dfbfc58a95fbcef804c9
Joshua Jacobs <josh@mandala-designs.com>
2011-08-03 11:56:16 -0400

    made scenario outline

--- features/step_definitions/submit_purchase_orders.rb
+++ features/step_definitions/submit_purchase_orders.rb
@@ -1,8 +1,8 @@
-Given /^the orders? (?:is|are) completed$/ do
+Given /^each order is completed$/ do
   Order.all.each {|order| complete_order order }
 end
 
-Given /^the orders? (?:has|have) (\d+) line items? with a quantity of (\d+)$/ do |item_count, quantity|
+Given /^each order has (\d) line item with a quantity of (\d)$/ do |item_count, quantity|
   skus = %w(9780373200009 978037352805 978037352812 978037352829)
   item_count = item_count.to_i - 1
   (0..item_count).each do |index|
@@ -17,14 +17,31 @@ When /^I create a purchase order$/ do
 
   @orders = []
   Order.needs_po.each {|o| @orders << o}
+
   @po_file = PoFile.generate
 end
 
-
-Then /^the purchase order should contain the (\d+)? ?given orders?$/ do |order_count|
+Then /^the purchase order should contain (\d+) orders?$/ do |order_count|
   order_count ||= 1
   @po_file.orders.count.should == order_count.to_i
   (@po_file.orders - @orders).size.should == 0
+end
+
+Then /^the purchase order file name should be formatted hb-YYMMDDHHMMSS.fbo$/ do
+  match = /^hb-(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2}).fbo/.match(@po_file.file_name)
+  match.should_not == nil
+  year, month, day, hour, min, sec = match.captures
+  time = @po_file.created_at
+  year.should == time.strftime("%y").to_s
+  month.should == time.strftime("%m").to_s
+  day.should == time.strftime("%d").to_s
+  hour.should == time.strftime("%H").to_s
+  min.should == time.strftime("%M").to_s
+  sec.should == time.strftime("%S").to_s
+
+  puts @po_file.data
+  
+  @po_file.delete_file
 end
 
 def complete_order(order)
