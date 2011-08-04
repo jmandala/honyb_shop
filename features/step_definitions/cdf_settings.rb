685b695be883b1f8f90d5bf0150912daf6863409
Joshua Jacobs <josh@mandala-designs.com>
2011-08-04 00:45:29 -0400

    modified Spree::Config[:key] to be Spree::Config.get(:key) because it seemed to help!

--- features/step_definitions/cdf_settings.rb
+++ features/step_definitions/cdf_settings.rb
@@ -1,7 +1,7 @@
 # define settings
 Given /^(CDF .*): (.*)$/ do |key, value|
   key = to_sym key
-  if Spree::Config[key] == ''
+  if Spree::Config.get(key).nil? || Spree::Config.get(key) == ''
     Spree::Config.set({key => value.strip})
   end
 end
