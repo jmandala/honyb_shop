1b63f601f9c0722c798d935639dc923fe82f9852
Joshua Jacobs <josh@mandala-designs.com>
2011-08-11 15:41:28 -0400

    added spec for cdf_binding

---
+++ spec/models/cdf_binding_code_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe CdfBindingCode do
+
+  context "when creating a new instance" do
+    before(:all) do
+      #noinspection RubyInstanceVariableNamingConvention
+      @mass_market = FactoryGirl.build :mass_market
+    end
+
+    it "should have a code" do
+      @mass_market.code.should == 'M'
+    end
+
+    it "should have a name" do
+      @mass_market.name.should == 'Mass Market'
+    end
+
+    it "should allow access to #other" do
+      CdfBindingCode.other.should_not == nil
+      CdfBindingCode.other.name == FactoryGirl.build(:other)
+    end
+
+
+  end
+
+
+end
