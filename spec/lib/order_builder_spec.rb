605078159d4b555fd0dd7dd8a0d2036afa515341
Joshua Jacobs <josh@mandala-designs.com>
2011-08-31 12:15:36 -0400

    - order_builder.rb: make it return a new test order - order_builder_spec.rb: test default test order attrs

--- spec/lib/order_builder_spec.rb
+++ spec/lib/order_builder_spec.rb
@@ -4,4 +4,13 @@ describe 'OrderBuilder' do
   it "should have a list of scenarios" do
     Cdf::OrderBuilder::SCENARIOS.should_not == nil
   end
+  
+  it "should create a new test order" do
+    order = Cdf::OrderBuilder.new_test
+    #noinspection RubyResolve
+    order.errors.should == {}
+    order.id.should_not == nil
+    order.id.should > 0
+  end
+  
 end
