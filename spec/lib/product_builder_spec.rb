5d603d7c10b010127ca8bc549303981b78ed942c
Joshua Jacobs <josh@mandala-designs.com>
2011-09-01 11:07:21 -0400

    - product_builder.rb: built out to create products by type - product_builder_spec.rb: better tests!

--- spec/lib/product_builder_spec.rb
+++ spec/lib/product_builder_spec.rb
@@ -3,8 +3,16 @@ require_relative '../spec_helper'
 describe Cdf::ProductBuilder do
   
   it "should create a new in-stock product" do
-    product = Cdf::ProductBuilder.in_stock!
+    product = Cdf::ProductBuilder.next_in_stock!
     product.should_not == nil
+  end
+  
+  it "should create new in stock products with new skus" do
+    sku1 = Cdf::ProductBuilder.next_sku Cdf::ProductBuilder::IN_STOCK
+    FactoryGirl.create(:product, :sku => sku1)
+    
+    sku2 = Cdf::ProductBuilder.next_sku Cdf::ProductBuilder::IN_STOCK
+    sku1.should_not == sku2
   end
   
 end
