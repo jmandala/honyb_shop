f5ef7506e3ce2e0d833b9031c6c5233ce9dd8b06
Joshua Jacobs <josh@mandala-designs.com>
2011-09-01 14:42:52 -0400

    - order_builder.rb: modified to use Cdf::ProductBuilder instance method - product_builder.rb: modified to use instance vars & method; #next_sku: modified to cycle through all the isbns, regardless of whether they have been used before; #create! modified to return product if it exists

--- spec/lib/product_builder_spec.rb
+++ spec/lib/product_builder_spec.rb
@@ -1,18 +1,23 @@
 require_relative '../spec_helper'
 
 describe Cdf::ProductBuilder do
-  
+
   it "should create a new in-stock product" do
-    product = Cdf::ProductBuilder.next_in_stock!
+    product = Cdf::ProductBuilder.new.next_in_stock!
     product.should_not == nil
   end
-  
+
   it "should create new in stock products with new skus" do
-    sku1 = Cdf::ProductBuilder.next_sku Cdf::ProductBuilder::IN_STOCK
-    FactoryGirl.create(:product, :sku => sku1)
-    
-    sku2 = Cdf::ProductBuilder.next_sku Cdf::ProductBuilder::IN_STOCK
-    sku1.should_not == sku2
+    pb = Cdf::ProductBuilder.new
+
+    pb.sku.each_key do |key|
+      2.times do
+        pb.sku[key].each do |isbn|
+          isbn.should == pb.next_sku(key)
+        end
+      end
+    end
+
   end
-  
+
 end
