e8a94b61b594da1d847e510ada158e733db2fefd
Joshua Jacobs <josh@mandala-designs.com>
2011-09-09 16:03:12 -0400

    more testing...

--- spec/controllers/admin/fulfillment/system_check_controller_spec.rb
+++ spec/controllers/admin/fulfillment/system_check_controller_spec.rb
@@ -5,7 +5,7 @@ describe Admin::Fulfillment::SystemCheckController do
   context "authorized user" do
     let(:user) { Factory(:admin_user) }
     before { controller.stub :current_user => user }
-    
+
 
     describe "GET 'index'" do
       it "should be successful" do
@@ -24,35 +24,48 @@ describe Admin::Fulfillment::SystemCheckController do
     describe "GET 'ftp_check'" do
       before(:each) do
         Cdf::Config.set({:cdf_run_mode => :mock})
-        get 'ftp_check'   
+        get 'ftp_check'
       end
-      
+
       it "assigns @valid_server" do
         assigns(:valid_server).should_not == nil
       end
-      
+
       it "assigns @valid_credentials" do
         assigns(:valid_credentials).should == true
       end
-      
+
       it "assigns @outgoing_files" do
-        assigns(:outgoing_files).should == []        
+        assigns(:outgoing_files).should == []
       end
-      
+
       it "assigns @test_files" do
-        assigns(:test_files).should == []        
+        assigns(:test_files).should == []
       end
-      
+
       it "assigns @archive_files" do
-        assigns(:archive_files).should == []        
+        assigns(:archive_files).should == []
       end
-      
+
       it "assigns @incoming_files" do
-        assigns(:incoming_files).should == []        
+        assigns(:incoming_files).should == []
       end
-      
+
       it "should be successful" do
         response.should be_success
+      end
+    end
+
+    describe "POST 'generate_test_order'" do
+      it "has value success" do
+        put :generate_test_orders, :scenarios => []
+        flash[:error].should == "No orders created"
+        response.should be_redirect
+      end
+      
+      it "assigns @orders" do
+        put :generate_test_orders, :scenarios => [1, 2, 3]
+        assigns(:orders).count.should == 3
       end
     end
   end
