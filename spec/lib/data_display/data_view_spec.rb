3c9ea04d95f2a23ccd5099cf79a6c94b35ea9e75
Joshua Jacobs <josh@mandala-designs.com>
2011-09-26 12:40:41 -0400

    initial data view handling

--- spec/lib/data_display/data_view_spec.rb
+++ spec/lib/data_display/data_view_spec.rb
@@ -4,14 +4,9 @@ describe DataDisplay::DataView do
   
   it 'should display a labeled pair' do
 
-    DataDisplay::DataView.data_pair('field_name', 'value')
-        
-"""
-.file_name
-  %label #{t 'file_name'}
-  = link_to poa_file.file_name, admin_fulfillment_poa_file_path(poa_file)
-"""    
-    
+    output = DataDisplay::DataView.data_pair('field_name')
+    output.gsub(/\s/, '').downcase.should == "<div class='field_name'><label>field_name</label></div>".gsub(/\s/, '')
+       
   end
   
 end
