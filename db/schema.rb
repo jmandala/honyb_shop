ee0eaaa9fd1e29ae0b7a321583d734f2e36110b2
Joshua Jacobs <josh@mandala-designs.com>
2011-09-29 09:14:28 -0400

    - zone_decorator.rb: Created with #includes_zoneable? to return true if the passed zoneable class is a member; and #self.all_us to return the ALL US zone - dashboard/index.html.haml: debugging to display all us states - State.txt: added AE, Armed Forces Middle East - shipping_methods.rb: created to store all shipping method information - tax_zones.rb: created to seed tax zones - zone_members.rb: created to seed zones - seeds.rb: added some debugging and extracted some groups to own files - countries.yml: added Minor Outlying Islands US, UMI - zone_members.yml: deleted - zones.yml: deleted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110922163742) do
+ActiveRecord::Schema.define(:version => 20110927191624) do
 
   create_table "address_types", :force => true do |t|
     t.string "code"
