8808b503e548131700bc3ffd03fca657d31aaae0
Joshua Jacobs <josh@mandala-designs.com>
2011-09-30 09:48:10 -0400

    - order_builder.rb: added ship locations; removed #us_address in favor of #create_address

--- spec/lib/order_builder_spec.rb
+++ spec/lib/order_builder_spec.rb
@@ -4,12 +4,11 @@ describe Cdf::OrderBuilder do
   before(:all) do
     @builder = Cdf::OrderBuilder
   end
-  
-  
+
   it "should have a list of scenarios" do
     @builder::SCENARIOS.should_not == nil
   end
-  
+
   it "should create a new test order" do
     order = @builder.completed_test_order
     #noinspection RubyResolve
@@ -24,41 +23,79 @@ describe Cdf::OrderBuilder do
     order.outstanding_balance.should == 0
     order.complete?.should == true
   end
-  
+
   it "should find the scenario by ID" do
     @builder.find_scenario(1)[:name].should == @builder::SCENARIOS[0][:name]
     @builder.find_scenario(2)[:name].should == @builder::SCENARIOS[1][:name]
   end
-  
+
   it "should create a new order by scenario number" do
     order = @builder.completed_test_order @builder.find_scenario(1)
     order.line_items.count.should == 1
     order.line_items[0].quantity.should == 1
-    order.should_not == nil    
+    order.should_not == nil
   end
-  
+
   it "should create a new order with quantity of two" do
     order = @builder.completed_test_order @builder.find_scenario(2)
     order.line_items.count.should == 1
     order.line_items[0].quantity.should == 2
   end
-  
+
   it "should create a new order with two line items" do
     order = @builder.completed_test_order @builder.find_scenario(3)
     order.line_items.count.should == 2
     order.line_items[0].quantity.should == 1
   end
-  
+
   it "should create a new order with two line items and quantity of two" do
     order = @builder.completed_test_order @builder.find_scenario(4)
     order.line_items.count.should == 2
-    order.line_items.each {|li| li.quantity.should == 2 }
+    order.line_items.each { |li| li.quantity.should == 2 }
   end
-  
+
   it "should create new orders for each scenario id in array" do
-    orders = @builder.create_for_scenarios [1,2,3]
+    orders = @builder.create_for_scenarios [1, 2, 3]
     orders.count.should == 3
   end
-  
-  
+
+  context "#create_address" do
+    it "requires a state argument" do
+      e = nil
+      begin
+        @builder.create_address
+      rescue => e
+        error = e
+      end
+
+      e.class.should == ArgumentError
+    end
+
+    it "should error when state is invalid" do
+      error = nil
+      begin
+        @builder.create_address :xyz
+      rescue => e
+        error = e
+      end
+
+      error.class.should == ActiveRecord::RecordNotFound
+      error.message.should == "Couldn't find State with abbr = xyz"
+    end
+
+    it "creates a domestic address" do
+      address = @builder.create_address :ME
+      address.state.abbr.should == 'ME'
+      address.country.iso3.should == 'USA'
+    end
+    
+    it "creates a foreign address" do
+      address = @builder.create_address :PR
+      address.state.abbr.should == 'PR'
+      address.country.iso3.should == 'PRI'
+      address.country.numcode.should == 630
+    end
+
+  end
+
 end
