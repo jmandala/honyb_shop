35e95dbe1d2772e1a952e2f29890eba4918d912c
Joshua Jacobs <josh@mandala-designs.com>
2011-10-21 21:57:29 -0400

    - asn_shipment_detail.rb: modified so that inventory_units are assigned first from the specified shipment, and second from the order; still need to calculate shipping costs accurately

--- spec/models/cdf_compliance_spec.rb
+++ spec/models/cdf_compliance_spec.rb
@@ -2,18 +2,17 @@ require_relative '../spec_helper'
 
 describe "CDF Compliance" do
 
-  before :each do
+  before :all do
+    AsnFile.all.each &:destroy
+    
     @builder = Cdf::OrderBuilder
-
-
-    @asn_file = AsnFile.create(:file_name => 'asn-test.txt')
-
   end
-
+  
+=begin
   context "should handle single order / single line / single quantity" do
 
     before :each do
-      AsnFile.all.each &:destroy
+    @asn_file = AsnFile.create(:file_name => 'asn-test.txt')
 
 
       @order = @builder.completed_test_order({:id => 1,
@@ -86,8 +85,10 @@ OD#{@order.number}            C 02367          0373200005037320000500001     000
 
     end
   end
+=end
 
-  context "should handle multiple boxes from single order" do
+=begin
+  context "should handle single order / single line / multiple quantity" do
 
     before :each do
       AsnFile.all.each &:destroy
@@ -166,12 +167,12 @@ OD#{@order.number.ljust_trim(22)}C 02367          0373200005037320000500002     
 
 
   end
-  context "should handle single order / single line / multiple quantity" do
 
-    before :each do
-      AsnFile.all.each &:destroy
+=end
 
+  context "should handle multiple boxes from single order" do
 
+    before :all do
       @order = @builder.completed_test_order({:id => 1,
                                               :name => 'single order/single lines/single quantity',
                                               :line_item_count => 1,
@@ -188,6 +189,7 @@ OD#{@order.number.ljust_trim(22)}C 02415          0373200005037320000500010     
 OD#{@order.number.ljust_trim(22)}C 02415          0373200005037320000500010     00010001ZTESTTRACKCI024150003   SCAC 1              00004990000289#{@line_item.id.to_s.ljust_trim(10)}TESTSSLCI0241500000400000020021#{@line_item.variant.sku.no_dashes.ljust_trim(15)}
       " ""
 
+      @asn_file = AsnFile.create(:file_name => 'asn-test-multibox.txt')      
       @asn_file.write_data(response)
       @asn_file.data.should == response
       @asn_file.import
@@ -195,9 +197,10 @@ OD#{@order.number.ljust_trim(22)}C 02415          0373200005037320000500010     
 
     it "should import the right number of shipments and shipment details" do
       @asn_file.asn_shipments.count.should == 1
-      @asn_file.asn_shipments.first.asn_shipment_details.count.should == 1
+      @asn_file.asn_shipments.first.asn_shipment_details.count.should == 3
     end
 
+=begin
     context "asn_shipment" do
       before :each do
         @asn_shipment = @asn_file.asn_shipments.first
@@ -210,40 +213,51 @@ OD#{@order.number.ljust_trim(22)}C 02415          0373200005037320000500010     
         @asn_shipment.asn_order_status.shipped?.should == true
       end
     end
+=end
 
     context "asn_shipment_detail" do
       before :each do
-        @asn_shipment_detail = @asn_file.asn_shipments.first.asn_shipment_details.first
+        @asn_shipment_details = @asn_file.asn_shipments.first.asn_shipment_details
       end
+
       it "should reference order" do
-        @asn_shipment_detail.order.should == @order
+        @asn_shipment_details.each { |s| s.order.should == @order }
+      end
+
+      it "should have the correct number of details" do
+        @asn_shipment_details.size.should == 3
       end
 
       it "should have correct quantities" do
-        @asn_shipment_detail.quantity_shipped.should == 2
-        @asn_shipment_detail.quantity_predicted.should == 2
-        @asn_shipment_detail.quantity_slashed.should == 0
+        @asn_shipment_details.each do |s|
+          s.quantity_shipped.should == 10
+          s.quantity_predicted.should == 10
+          s.quantity_slashed.should == 0
+          s.inventory_units.count.should == 10
+        end
       end
 
       it "should have shipped status" do
-        @asn_shipment_detail.asn_order_status.shipped?.should == true
+        @asn_shipment_details.each do |s|
+          s.asn_order_status.shipped?.should == true
+        end
       end
 
       it "should reference the line item from the order" do
-        @asn_shipment_detail.line_item.should_not == nil
+        @asn_shipment_details.each do |s|
+          s.line_item.should_not == nil
+        end
       end
 
-      it "should have 2 inventory units" do
-        @asn_shipment_detail.inventory_units.count.should == 2
-        @asn_shipment_detail.shipment.inventory_units.count.should == 2
-      end
 
       it "should be shipped" do
-        @asn_shipment_detail.shipment.state.should == 'shipped'
-        @asn_shipment_detail.inventory_units.first.state.should == 'shipped'
-        @asn_shipment_detail.shipment.tracking.should_not == nil
-      end
+        @asn_shipment_details.each do |s|
+          s.shipment.state.should == 'shipped'
+          s.inventory_units.first.state.should == 'shipped'
+          s.shipment.tracking.should_not == nil
+        end
 
+      end
     end
 
 
