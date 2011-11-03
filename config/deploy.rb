d5b605e6504ef1ad3b5e7039a37e38e3cee6332b
Joshua Jacobs <josh@mandala-designs.com>
2011-11-03 14:12:44 -0400

    removed --deployment flag

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,5 +49,5 @@ after 'deploy:update_code', :bundle_install
 
 desc 'install the necessary prerequisites'
 task :bundle_install, :roles => :app do
-  run "cd #{release_path} && bundle install --deployment"
+  run "cd #{release_path} && bundle install"
 end
