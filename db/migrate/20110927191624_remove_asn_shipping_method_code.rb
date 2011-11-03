41021299e4e50880c303e37206c6c1e0aab82bff
Joshua Jacobs <josh@mandala-designs.com>
2011-11-03 14:49:13 -0400

    revised migrat8ion

--- db/migrate/20110927191624_remove_asn_shipping_method_code.rb
+++ db/migrate/20110927191624_remove_asn_shipping_method_code.rb
@@ -1,9 +1,9 @@
 class RemoveAsnShippingMethodCode < ActiveRecord::Migration
   def self.up
-    remove_column :asn_shipment_details, :asn_shipping_method_code
+    #remove_column :asn_shipment_details, :asn_shipping_method_code
   end
 
   def self.down
-    add_column :asn_shipment_details, :asn_shipping_method_code, :integer
+    #add_column :asn_shipment_details, :asn_shipping_method_code, :integer
   end
 end
