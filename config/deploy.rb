857e72a946a6e1d49e91e1ee9058706c68d777b8
Joshua Jacobs <josh@mandala-designs.com>
2011-11-04 11:00:35 -0400

    add tail_logs task to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,3 +57,12 @@ task :bundle_install, :roles => :app do
   run "cd #{release_path} && gem install sqlite3 -- --with-sqlite3-dir=/opt/local/sqlite-3.7.0.9"
   run "cd #{release_path} && bundle install"
 end
+
+desc "tail production log files" 
+task :tail_logs, :roles => :app do
+  run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+    puts  # for an extra line break before the host name
+    puts "#{channel[:host]}: #{data}" 
+    break if stream == :err    
+  end
+end
