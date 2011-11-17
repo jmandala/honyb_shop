4d721340cd77b39441e15c24df383cd1247bcf6e
Joshua Jacobs <josh@mandala-designs.com>
2011-11-17 16:28:00 -0500

    got fulfillment link working in admin order tabs

--- cdf/app/overrides/admin_order_tabs.rb
+++ cdf/app/overrides/admin_order_tabs.rb
@@ -1,8 +1,9 @@
-Deface::Override.new(:virtual_path => "layouts/admin",
+Deface::Override.new(:virtual_path => "admin/shared/_order_tabs",
                      :name => "admin_order_tabs",
                      :insert_bottom => "[data-hook='admin_order_tabs'], #admin_tabs[data-hook]",
-                     :text =>   %(<li<%== ' class="active"' if current == "Fulfillment" %>>
-       <%= link_to t(:fulfillment), fulfillment_admin_order_path(@order) %> ???
+                     :text => %( <% if @order.completed? %>
+<li<%== ' class="active"' if current == "Fulfillment" %>>
+       <%= link_to t(:fulfillment), fulfillment_admin_order_path(@order) %>
     </li>
-    ),
-                     :disabled => false)
+<% end %>
+))
