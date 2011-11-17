75e88f21dc235eff9fd7738f6785ad25237d793b
Joshua Jacobs <josh@mandala-designs.com>
2011-11-17 16:39:14 -0500

    converting haml to erb

--- cdf/app/overrides/admin_order_show_buttons.rb
+++ cdf/app/overrides/admin_order_show_buttons.rb
@@ -1,4 +1,3 @@
-
   #insert_after :admin_order_show_buttons, 'admin/shared/order_details_extra_buttons'
   Deface::Override.new(:virtual_path => "admin/orders/show",
                        :name => "converted_admin_order_show_buttons_435560197",
