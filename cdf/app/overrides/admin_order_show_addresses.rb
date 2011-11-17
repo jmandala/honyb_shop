65aca328480c75d2776eec99d79d051bca49e206
Joshua Jacobs <josh@mandala-designs.com>
2011-11-17 17:12:42 -0500

    order name

--- cdf/app/overrides/admin_order_show_addresses.rb
+++ cdf/app/overrides/admin_order_show_addresses.rb
@@ -1,8 +1,6 @@
-
-
   #insert_before :admin_order_show_addresses, 'admin/shared/order_details_before_addresses'
   Deface::Override.new(:virtual_path => "admin/orders/show",
                        :name => "converted_admin_order_show_addresses_756039867",
                        :insert_before => "[data-hook='admin_order_show_addresses'], #admin_order_show_addresses[data-hook]",
-                       :partial => "admin/shared/order_details_before_addresses",
+                       :text => "<% unless @order.order_name.blank? %><h2><%= @order.order_name %><% end %>",
                        :disabled => false)
