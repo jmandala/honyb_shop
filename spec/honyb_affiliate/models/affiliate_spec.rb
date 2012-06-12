4a1a574e1b482b30a8689192bef86a792103a734
Joshua Jacobs <josh@mandala-designs.com>
2012-06-12 15:27:44 -0400

    Created Affiliate Migration and began implementing Affiliate#init - affiliate.rb: #init raises exception if honyb_id is not found

--- spec/honyb_affiliate/models/affiliate_spec.rb
+++ spec/honyb_affiliate/models/affiliate_spec.rb
@@ -5,5 +5,19 @@ describe Affiliate, :type => :model do
   it "#current should return nil by default" do
     Affiliate.current.should == nil
   end
-  
+
+  context "when params are invalid" do
+    let(:honyb_id) {'invalid'}
+    
+    it "#init should throw an exception" do
+      begin
+        Affiliate.init({:honyb_id => honyb_id}, {})
+        raise StandardError, "should not get here"
+      rescue ActiveRecord::RecordNotFound => e
+        puts e.message
+        e.message.should == %Q{Couldn't find Affiliate with honyb_id = #{honyb_id}}
+      end
+    end
+  end
+
 end
