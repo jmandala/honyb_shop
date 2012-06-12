b5cfa31f3a9058a0c8ad35fff07ed607e6373223
Joshua Jacobs <josh@mandala-designs.com>
2012-06-12 15:53:37 -0400

    Affiliate#init now sets the values in Affiliate#current or throws an exception when the honyb_id is invalid - affiliate.rb: #init not takes a single string parameter and doesn't know anything about the session - base_controller_decorator.rb: #init_affiliate now passes that param if available, otherwise the session, and sets the session if an Affiliate was set

---
+++ spec/honyb_affiliate/controllers/base_controller_decorator_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe HomeController, :type => :controller do
+  let(:honyb_id) { 'the-affiliate-id' }
+
+  before do
+    controller.stub :current_user => nil
+  end
+
+  it "initializes the affiliate on every request" do
+    Affiliate.should_receive(:init).with(nil)
+
+    get :index
+    Affiliate.current.should == nil
+    request.session[:honyb_id].should == nil
+  end
+
+  context "invalid honyb_id" do
+
+    it "should raise error with invalid param data" do
+      begin
+        get :index, :honyb_id => honyb_id
+      rescue ActiveRecord::RecordNotFound => e
+        e.message.should == %Q{Couldn't find Affiliate with honyb_id = #{honyb_id}}
+      end
+    end
+
+    it "should raise error with invalid session data" do
+      begin
+        request.session[:honyb_id] = honyb_id
+        get :index
+      rescue ActiveRecord::RecordNotFound => e
+        e.message.should == %Q{Couldn't find Affiliate with honyb_id = #{honyb_id}}
+      end
+    end
+
+  end
+  
+  context "valid honyb_id" do
+    before do
+      @affiliate = Affiliate.create(:honyb_id => honyb_id)
+    end
+    
+    it "should get the affiliate with the param data" do
+      get :index, :honyb_id => honyb_id
+      Affiliate.current.should == @affiliate
+      request.session[:honyb_id].should == honyb_id
+    end
+    
+    it "should get the affiliate with the session data" do
+      request.session[:honyb_id] = honyb_id
+      get :index
+      Affiliate.current.should == @affiliate
+      request.session[:honyb_id].should == honyb_id
+    end
+  end
+end
--- spec/honyb_affiliate/models/affiliate_spec.rb
+++ spec/honyb_affiliate/models/affiliate_spec.rb
@@ -1,23 +1,36 @@
 require "spec_helper"
 
 describe Affiliate, :type => :model do
+  let(:honyb_id) {'affiliate-id'}
 
   it "#current should return nil by default" do
     Affiliate.current.should == nil
   end
 
   context "when params are invalid" do
-    let(:honyb_id) {'invalid'}
     
     it "#init should throw an exception" do
       begin
-        Affiliate.init({:honyb_id => honyb_id}, {})
+        Affiliate.init(honyb_id)
         raise StandardError, "should not get here"
       rescue ActiveRecord::RecordNotFound => e
-        puts e.message
+        Affiliate.has_current?.should == false
         e.message.should == %Q{Couldn't find Affiliate with honyb_id = #{honyb_id}}
       end
     end
   end
 
+  context "when params are valid" do
+    before do
+      @affiliate = Affiliate.create(:honyb_id => honyb_id)
+    end
+    
+    it "#init should set the correct affiliate" do
+      Affiliate.init(honyb_id)
+      Affiliate.current.should == @affiliate
+      Affiliate.has_current?.should == true
+    end
+    
+  end
+  
 end
