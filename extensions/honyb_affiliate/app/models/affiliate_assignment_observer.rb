15331d638a37225b6f99477cebb33f1767f1694b
Joshua Jacobs <josh@mandala-designs.com>
2012-06-20 10:33:18 -0400

    Added AffiliateAssignmentObserver which will add the current affiliate to the order upon create - affiliate_assignment_observer.rb: created - affiliate_factory.rb: created - application.rb: added observer to config

---
+++ extensions/honyb_affiliate/app/models/affiliate_assignment_observer.rb
@@ -0,0 +1,13 @@
+class AffiliateAssignmentObserver < ActiveRecord::Observer
+  observe :order
+  
+  # If there is a current affiliate, and the order doesn't yet have an affifliate assigned, 
+  # assign it
+  def after_create(order)
+    if Affiliate.current.nil? || order.affiliate
+      return
+    end
+    
+    order.affiliate = Affiliate.current
+  end
+end
