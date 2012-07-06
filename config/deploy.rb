0ba6a58ec0e31eb87332aeb8d76602246612efc1
Joshua Jacobs <josh@mandala-designs.com>
2012-07-05 23:03:43 -0400

    updated deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,11 @@
 require 'bundler/capistrano'
 require "delayed/recipes"
 
-#set :application, "honyb_shop"
+#set :stages, %w(stage production)
+set :application, "honyb_shop"
 #set :domain, 'www.honyb.com'
 #set :host, 'ruby.mandaladesigns.com'
 #set :asset_env, '--trace'
-
 
 set :scm, :git
 set :git_server, 'code.mandaladesigns.com'
@@ -22,9 +22,9 @@ set :user, 'honyb'
 set :group, "honyb"
 set :use_sudo, false
 
-role :web, "www2.honyb.com"
-role :app, "www2.honyb.com"
-role :db, "www2.honyb.com", :primary => true # This is where Rails migrations will run
+role :web, "www1.honyb.com", "www2.honyb.com"
+role :app, "www1.honyb.com", "www2.honyb.com"
+role :db, "www1.honyb.com", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 set(:latest_release) { fetch(:current_path) }
@@ -43,24 +43,9 @@ default_environment["GEM_HOME"] = "/usr/local/rvm/gems/ruby-1.9.2-p290"
 default_environment["GEM_PATH"] = "/usr/local/rvm/gems/ruby-1.9.2-p290:/usr/local/rvm/gems/ruby-1.9.2-p290@global"
 default_environment["RUBY_VERSION"] = "ruby-1.9.2-p290"
 
-# adjust if you are using RVM, remove if you are not
-#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-#require 'rvm/capistrano'
-#set :rvm_ruby_string, 'ruby-1.9.2-p290@rails-3.1.1'
-
 default_run_options[:shell] = 'bash'
 
 set :scm_verbose, true
-
-set :keep_releases, '10'
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-
-end
 
 namespace :deploy do
   desc "Deploy your application"
