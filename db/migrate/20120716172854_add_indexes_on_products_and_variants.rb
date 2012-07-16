1e654a63496cb59f3aedec47f1275d393f3c10bc
Joshua Jacobs <josh@mandala-designs.com>
2012-07-16 14:35:11 -0400

    added multi-index on products

--- db/migrate/20120716172854_add_indexes_on_products_and_variants.rb
+++ db/migrate/20120716172854_add_indexes_on_products_and_variants.rb
@@ -1,6 +1,5 @@
 class AddIndexesOnProductsAndVariants < ActiveRecord::Migration
   def change
-    rename_index :products, :index_products_on_name, :name
     add_index :variants, :deleted_at
     add_index :variants, :is_master
   end
---
+++ db/migrate/20120716182949_add_multi_index_on_products.rb
@@ -0,0 +1,5 @@
+class AddMultiIndexOnProducts < ActiveRecord::Migration
+  def change
+    add_index :products, :deleted_at_and_name, [deleted_at, name]
+  end
+end
