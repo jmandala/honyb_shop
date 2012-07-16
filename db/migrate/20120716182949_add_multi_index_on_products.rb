668d8b5cf031369b1aa63531a720b964c607639e
Joshua Jacobs <josh@mandala-designs.com>
2012-07-16 14:40:28 -0400

    fixed typo in migration

--- db/migrate/20120716182949_add_multi_index_on_products.rb
+++ db/migrate/20120716182949_add_multi_index_on_products.rb
@@ -1,5 +1,5 @@
 class AddMultiIndexOnProducts < ActiveRecord::Migration
   def change
-    add_index :products, :deleted_at_and_name, [deleted_at, name]
+    add_index :products, :deleted_at_and_name, [:deleted_at, :name]
   end
 end
