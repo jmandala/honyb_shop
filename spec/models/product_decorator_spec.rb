b22cc2e304fb3c354f91e41a6623bf8f6259e173
aalvarado <adan.alvarado7@gmail.com>
2015-05-08 07:41:17 -0700

    Fix Google books product description example

--- spec/models/product_decorator_spec.rb
+++ spec/models/product_decorator_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'product' do
 
   let(:product) { Product.new }
-  
+
   it "should specify the correct SKU TYPE" do
     product.sku_type.should == 'EN'
   end
-  
+
   it "should run the class method" do
     Product.spec_test.should == true
   end
@@ -19,8 +19,8 @@ describe 'product' do
 
     product.name.should == "The War Within"
     product.subtitle.should == "A Novel of the Civil War"
-    product.description.should == "In 1862, after Union forces expel Hannah's family from Holly Springs, Mississippi, because they are Jews, Hannah reexamines her views regarding slavery and the war."
-    product.page_count.should == 151
+    product.description.should == "Holly Springs, Mississippi, 1862 Hannah Green can't believe what happens to her family after the war breaks out. First, her sister Joanna falls in love with a Union soldier -- an enemy. Next, the same soldier tells Hannah and her family about General Grant's General Order #11, which commands all Jews to evacuate the territory for violating trade regulations. The Greens escape from Holly Springs just before their home is destroyed. They lose everything -- even their slaves, when Lincoln declares them free. Now, because she is Jewish, Hannah cannot go home to Mississippi -- a Confederate state that's dear to her heart. Confusion sets in. Who is on her side, and whose side does she want to be on?"
+    product.page_count.should == 160
     product.publisher.should == "Aladdin"
     product.published_date.strftime("%Y-%m-%d").to_s.should == Date.parse("2002-09-01").to_s
     product.images.size.should == 1
@@ -29,6 +29,6 @@ describe 'product' do
     product.raw_biblio_info["volumeInfo"]["title"].should == product.name
     product.google_books_update.should_not == nil
   end
-  
-  
+
+
 end
