1296b77dc4d8d9d511e12f3b8da306dd4046cb60
aalvarado <adan.alvarado7@gmail.com>
2015-05-08 07:37:24 -0700

    Remove honyb reports

--- extensions/honyb_reports/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  
-  match '/admin/reports/sales_detail' => 'admin/reports#sales_detail',  :via  => [:get, :post],
-                                                              :as   => 'sales_detail_admin_reports'
-  
-end
